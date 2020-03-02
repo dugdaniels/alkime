70301a21209d4423b98c8d01ebcc05b82ef64eff
Corey Daniels <cdouglasd@gmail.com>
2020-03-02 10:48:18 -0500

    Add CNAME

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,12 @@ import PropTypes from "prop-types"
 import { jsx, css } from "@emotion/core"
 
 const style = css`
-  height: 100vh;
+  height: 50vh;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: sans-serif;
 `
 
 const Layout = ({ children }) => {
