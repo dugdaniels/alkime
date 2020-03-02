93ad3772172aee909f8a3c20e355eb8534c3886c
Corey Daniels <cdouglasd@gmail.com>
2020-03-02 10:25:43 -0500

    Setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,47 +1,17 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from "react"
+/** @jsx jsx */
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { jsx, css } from "@emotion/core"
 
-import Header from "./header"
-import "./layout.css"
+const style = css`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+  return <main css={style}>{children}</main>
 }
 
 Layout.propTypes = {
