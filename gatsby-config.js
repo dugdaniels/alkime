cd5a9673908d561bedeb8eaefbb121779361c5cd
Corey Daniels <cdouglasd@gmail.com>
2020-03-02 10:56:02 -0500

    Meta updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Alkime Softworks`,
+    description: `A digital design and development firm located in Charlotte, NC.`,
     author: `@gatsbyjs`,
   },
   plugins: [
